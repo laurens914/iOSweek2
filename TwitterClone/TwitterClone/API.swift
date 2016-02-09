652066fa2220a4343878a1246bd44296f24c7691
Branton Boehm <branton@outlook.com>
2016-02-09 13:04:28 -0800

    Implementing API.updateTimeline function.

--- TwitterClone/TwitterClone/API.swift
+++ TwitterClone/TwitterClone/API.swift
@@ -42,5 +42,35 @@ class API {
         }
     }
 
-    private func updateTimeline() {}
+    private func updateTimeline(completion: (tweets: [Tweet]?) -> ()) {
+        let request = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: NSURL(string: "https://api.twitter.com/1.1/statuses/home_timeline.json"), parameters: nil)
+        request.account = self.account
+        request.performRequestWithHandler { (data: NSData!, response: NSHTTPURLResponse!, error: NSError!) -> Void in
+            var tweets: [Tweet]?
+
+            switch response.statusCode {
+            case 100...199:
+                print("Informational: \(response.statusCode)")
+            case 200...299:
+                print("Success: \(response.statusCode)")
+                JSONParser.tweetJSONFrom(data, completion: { (success: Bool, parsedTweets: [Tweet]?) -> () in
+                    guard success else {
+                        print("JSON parsing failed.")
+                        return
+                    }
+                    tweets = parsedTweets
+                })
+            case 300...399:
+                print("Redirect: \(response.statusCode)")
+            case 400...499:
+                print("Client error: \(response.statusCode)")
+            case 500...599:
+                print("Server error: \(response.statusCode)")
+            default:
+                print("Unknown response: \(response.statusCode)")
+            }
+
+            completion(tweets: tweets)
+        }
+    }
 }
