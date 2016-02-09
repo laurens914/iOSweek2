aef13b1d403d119070d062308d398473e3c0aabc
Branton Boehm <branton@outlook.com>
2016-02-09 12:17:23 -0800

    Adding API.swift and functions.

---
+++ TwitterClone/TwitterClone/API.swift
@@ -0,0 +1,22 @@
+//
+//  API.swift
+//  TwitterClone
+//
+//  Created by Branton Boehm on 2/9/16.
+//  Copyright © 2016 Lauren Spatz. All rights reserved.
+//
+
+import Foundation
+import Accounts
+import Social
+
+class API {
+    static let shared = API()
+    private init() {}
+
+    func getTweets() {}
+    func getUser() {}
+
+    private func login() {}
+    private func updateTimeline() {}
+}
